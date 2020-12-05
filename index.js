175fe04edfdbccbb2b483321c086451e12309463
bgleoquip <bgleoquip@gmail.com>
2020-12-05 18:25:25 +0530

    cors

--- index.js
+++ index.js
@@ -9,14 +9,15 @@ const app = express();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-// app.use(cors());
+app.use(cors());
 
-app.use(
-  cors({
-    origin: "https://dashboard.heroku.com/apps/bg-mern-back",
-    credentials: true
-  })
-);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/posts', postRoutes);
 
 const CONNECTION_URL = process.env.MONGOURL || 'mongodb+srv://bgbhavesh:bgleoquip@bgcluster.vwvhb.mongodb.net/name?retryWrites=true&w=majority';
